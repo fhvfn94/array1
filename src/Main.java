0219f76352480e0ef73d2b68c1b0a65e6907d4ab
fhvfn94 <fhvfn94@gmail.com>
2022-10-15 21:25:59 +0300

    add task 1, 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //example();
         //task1();
-        task2();
+        //task2();
+        task3();
     }
     public static void example(){
         int[] weights = new int[12];
@@ -27,14 +28,21 @@ public class Main {
     public static void task2(){
         int[] arr1 = {1, 2, 3};
         for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + "; ");
+            System.out.print(arr1[i] + " ");
         }
+        System.out.println();
         double[] arr2 = {1.57, 7.654, 9.986};
         for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + "; ");
+            System.out.print(arr2[i] + " ");
         }
-        boolean[] arr3 = new boolean[2];
-        arr3[0] = true;
-        System.out.println(Arrays.toString(arr3));
+        System.out.println();
+        int[] arr3 = new int[2];
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void task3() {
+
     }
 }
