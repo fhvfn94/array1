9bb3560aa11267b4510a82535799e3cd56ead440
fhvfn94 <fhvfn94@gmail.com>
2022-10-16 11:52:22 +0300

    add task 1, 2, 3

--- src/Main.java
+++ src/Main.java
@@ -28,36 +28,57 @@ public class Main {
     public static void task2(){
         int[] arr1 = {1, 2, 3};
         for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + " ");
+            System.out.print(arr1[i]);
+            if (arr1.length > arr1[i]){
+                System.out.print(", ");
+            }
         }
         System.out.println();
         double[] arr2 = {1.57, 7.654, 9.986};
         for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
+            System.out.print(arr2[i]);
+            if (arr2.length - 1 == i){
+            } else {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         int[] arr3 = new int[2];
         for (int i = 0; i < arr3.length; i++) {
-            System.out.print(arr3[i] + " ");
+            System.out.print(arr3[i]);
+            if (arr3.length - 1 == i){
+            } else {
+                System.out.print(", ");
+            }
+
         }
         System.out.println();
     }
     public static void task3() {
         int[] arr1 = {1, 2, 3};
         for (int i = arr1.length - 1; i >= 0; i--) {
-            System.out.print(arr1[i] + " ");
+                System.out.print(arr1[i]);
+                if (arr1.length - 1 >= i){
+                    System.out.print(", ");
+                }
         }
         System.out.println();
         double[] arr2 = {1.57, 7.654, 9.986};
         for (int i = arr1.length - 1; i >= 0; i--) {
-            System.out.print(arr2[i] + " ");
+            System.out.print(arr2[i]);
+            if (arr2.length - 1 >= i){
+                System.out.print(", ");
+            }
         }
         System.out.println();
         int[] arr3 = new int[2];
         arr3[0] = 20;
         arr3[1] = 10;
         for (int i = arr3.length - 1; i >= 0; i--) {
-            System.out.print(arr3[i] + " ");
+            if (arr3.length - 1 > i){
+                System.out.print(", ");
+            }
+            System.out.print(arr3[i]);
         }
         System.out.println();
     }
